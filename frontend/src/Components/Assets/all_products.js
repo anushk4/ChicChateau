502c9f2abf92dac5680cd82c7cd50ae70eb301aa
Anushka Srivastava <anushka22086@iiitd.ac.in>
2024-06-03 14:46:22 +0530

    category pages

---
+++ frontend/src/Components/Assets/all_products.js
@@ -0,0 +1,329 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+import p5_img from './product_5.png'
+import p6_img from './product_6.png'
+import p7_img from './product_7.png'
+import p8_img from './product_8.png'
+import p9_img from './product_9.png'
+import p10_img from './product_10.png'
+import p11_img from './product_11.png'
+import p12_img from './product_12.png'
+import p13_img from './product_13.png'
+import p14_img from './product_14.png'
+import p15_img from './product_15.png'
+import p16_img from './product_16.png'
+import p17_img from './product_17.png'
+import p18_img from './product_18.png'
+import p19_img from './product_19.png'
+import p20_img from './product_20.png'
+import p21_img from './product_21.png'
+import p22_img from './product_22.png'
+import p23_img from './product_23.png'
+import p24_img from './product_24.png'
+import p25_img from './product_25.png'
+import p26_img from './product_26.png'
+import p27_img from './product_27.png'
+import p28_img from './product_28.png'
+import p29_img from './product_29.png'
+import p30_img from './product_30.png'
+import p31_img from './product_31.png'
+import p32_img from './product_32.png'
+import p33_img from './product_33.png'
+import p34_img from './product_34.png'
+import p35_img from './product_35.png'
+import p36_img from './product_36.png'
+
+let all_product = [
+    {
+        id:1,
+        name: "High Neck Cuffed Sleeve Satin Regular Top",
+        image: p1_img,
+        new_price: 1670,
+        old_price: 2695,
+        category: "women",
+    },
+    {
+        id:2,
+        name: "Single-Breasted Blazer With Matching Belted Shorts Co-Ords",
+        image: p2_img,
+        new_price: 724,
+        old_price: 3149,
+        category: "women",
+    },
+    {
+        id:3,
+        name: "Black Self-Design Craftcore Top",
+        image: p3_img,
+        new_price: 659,
+        old_price: 1999,
+        category: "women",
+    },
+    {
+        id:3,
+        name: "Pure Cotton Long Sleeves Brand Logo Printed Sweatshirt",
+        image: p4_img,
+        new_price: 1034,
+        old_price: 2299,
+        category: "women",
+    },
+    {
+        id:5,
+        name: "Tie and Dye Drop-Shoulder Sleeves Loose Pure Cotton T-shirt",
+        image: p5_img,
+        new_price: 503,
+        old_price: 1199,
+        category: "men",
+    },
+    {
+        id:6,
+        name: "Beige Typography Printed Shirt Collar Pure Cotton Playsuit",
+        image: p6_img,
+        new_price: 2431,
+        old_price: 3799,
+        category: "women",
+    },
+    {
+        id:7,
+        name: "Girls Round Neck Fit and Flare Net Dress",
+        image: p7_img,
+        new_price: 674,
+        old_price: 2249,
+        category: "kids",
+    },
+    {
+        id:8,
+        name: "Puffer Gilet",
+        image: p8_img,
+        new_price: 1499,
+        old_price: 2499,
+        category: "women",
+    },
+    {
+        id:9,
+        name: "Spread Collar Checked Cotton Casual Shirt",
+        image: p9_img,
+        new_price: 895,
+        old_price: 1599,
+        category: "men",
+    },
+    {
+        id:10,
+        name: "Knitted Casual Shirt",
+        image: p10_img,
+        new_price: 2399,
+        old_price: 3999,
+        category: "men",
+    },
+    {
+        id:11,
+        name: "Boys Graphic Printed Cotton T-shirt",
+        image: p11_img,
+        new_price: 599,
+        old_price: 799,
+        category: "kids",
+    },
+    {
+        id:12,
+        name: "Girls Embellished Ready to Wear Lehenga & Blouse With Dupatta",
+        image: p12_img,
+        new_price: 1439,
+        old_price: 3599,
+        category: "kids",
+    },
+    {
+        id:13,
+        name: "Floral Printed Bell Sleeve Top",
+        image: p13_img,
+        new_price: 1650,
+        old_price: 2750,
+        category: "women",
+    },
+    {
+        id:14,
+        name: "Floral Print Halter Neck Top",
+        image: p14_img,
+        new_price: 2274,
+        old_price: 3790,
+        category: "women",
+    },
+    {
+        id:15,
+        name: "Abstract Printed One Shoulder Top",
+        image: p15_img,
+        new_price: 879,
+        old_price: 1599,
+        category: "women",
+    },
+    {
+        id:16,
+        name: "Collared Zip-Front Vest Top",
+        image: p16_img,
+        new_price: 799,
+        old_price: 999,
+        category: "women",
+    },
+    {
+        id:17,
+        name: "White Floral Printed Puff Sleeves V-Neck Playsuit",
+        image: p17_img,
+        new_price: 4799,
+        old_price: 11999,
+        category: "women",
+    },
+    {
+        id:18,
+        name: "Brand Logo Printed Pure Cotton T-shirt",
+        image: p18_img,
+        new_price: 534,
+        old_price: 890,
+        category: "women",
+    },
+    {
+        id:19,
+        name: "Slim Fit Pure Cotton Checked Formal Shirt",
+        image: p19_img,
+        new_price: 1124,
+        old_price: 2499,
+        category: "men",
+    },
+    {
+        id:20,
+        name: "Stand Collar Tailored Jacket",
+        image: p20_img,
+        new_price: 3395,
+        old_price: 6790,
+        category: "men",
+    },
+    {
+        id:21,
+        name: "Loose Fit Short-Sleeved Linen-Blend Shirt",
+        image: p21_img,
+        new_price: 1499,
+        old_price: 3499,
+        category: "men",
+    },
+    {
+        id:22,
+        name: "Slim Fit Multi Striped Cotton Casual Shirt",
+        image: p22_img,
+        new_price: 666,
+        old_price: 2299,
+        category: "men",
+    },
+    {
+        id:23,
+        name: "Solid Textured Polo T-Shirts",
+        image: p23_img,
+        new_price: 1394,
+        old_price: 1499,
+        category: "men",
+    },
+    {
+        id:24,
+        name: "Breathe & Buzz Fleece Hooded Sweatshirt",
+        image: p24_img,
+        new_price: 2414,
+        old_price: 3449,
+        category: "men",
+    },
+    {
+        id:25,
+        name: "Men Black Relaxed Fit Sweatshirt",
+        image: p25_img,
+        new_price: 799,
+        old_price: 1999,
+        category: "men",
+    },
+    {
+        id:26,
+        name: "Men Slim Fit Fine-Knit Turtleneck Jumper",
+        image: p26_img,
+        new_price: 1099,
+        old_price: 1999,
+        category: "men",
+    },
+    {
+        id:27,
+        name: "Men Printed Mandarin Collar Sherwani Set",
+        image: p27_img,
+        new_price: 17596,
+        old_price: 21995,
+        category: "men",
+    },
+    {
+        id:28,
+        name: "Girls Flared-Skirt Dress",
+        image: p28_img,
+        new_price: 1049,
+        old_price: 1499,
+        category: "kids",
+    },
+    {
+        id:29,
+        name: "Boys Black & White Shirt with Shorts",
+        image: p29_img,
+        new_price: 874,
+        old_price: 3499,
+        category: "kids",
+    },
+    {
+        id:30,
+        name: "Boys Colourblocked Cotton T-shirt",
+        image: p30_img,
+        new_price: 489,
+        old_price: 999,
+        category: "kids",
+    },
+    {
+        id:31,
+        name: "Boys Black Pure Cotton Regular T-shirt",
+        image: p31_img,
+        new_price: 349,
+        old_price: 699,
+        category: "kids",
+    },
+    {
+        id:32,
+        name: "Girls Black Ribbed Cotton Vest Top",
+        image: p32_img,
+        new_price: 399,
+        old_price: 599,
+        category: "kids",
+    },
+    {
+        id:33,
+        name: "Girls Square Neck Sleeveless Pure Cotton Top",
+        image: p33_img,
+        new_price: 599,
+        old_price: 1499,
+        category: "kids",
+    },
+    {
+        id:34,
+        name: "Kids Monochrome T-shirt",
+        image: p34_img,
+        new_price: 599,
+        old_price: 1499,
+        category: "kids",
+    },
+    {
+        id:35,
+        name: "Girls Drawstring T-Shirt",
+        image: p35_img,
+        new_price: 319,
+        old_price: 799,
+        category: "kids",
+    },
+    {
+        id:36,
+        name: "Boys Printed Pure Cotton Vest Top",
+        image: p36_img,
+        new_price: 249,
+        old_price: 499,
+        category: "kids",
+    }
+]
+
+export default all_product;
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div>
+      {/* https://youtu.be/y99YgaQjgx4?si=086Cnrj8BJq49zdg&t=5953 */}
+    </div>
   )
 }
 
